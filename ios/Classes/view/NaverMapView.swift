c7c6fffbea265280638b90a2d43c0a04f4b1c103
Logan <152923750+EarthEmpathy@users.noreply.github.com>
2025-05-18 05:40:35 +0900

    fix(iOS): NaverMapView.swift

--- ios/Classes/view/NaverMapView.swift
+++ ios/Classes/view/NaverMapView.swift
@@ -8,7 +8,7 @@ import UIKit
 import NMapsMap
 
 internal class NaverMapView: NSObject, FlutterPlatformView {
-    // 1) Dart ↔ Native 통신 채널을 저장할 프로퍼티 추가
+    // 1) Dart ↔ Native 통신 채널 저장
     private let channel: FlutterMethodChannel
     // 기존 프로퍼티들
     private let naverMap: NMFNaverMapView!
@@ -16,7 +16,7 @@ internal class NaverMapView: NSObject, FlutterPlatformView {
     private let naverMapControlSender: NaverMapControlSender
     private var eventDelegate: NaverMapViewEventDelegate!
 
-    // 2) init 시 channel을 받고 프로퍼티에 할당
+    // 2) init 시 channel을 전달받아 저장
     init(frame: CGRect,
          options: NaverMapViewOptions,
          channel: FlutterMethodChannel,
@@ -32,18 +32,19 @@ internal class NaverMapView: NSObject, FlutterPlatformView {
             channel: channel,
             overlayController: overlayController
         )
+
         super.init()
 
-        // 3) 기존 옵션 적용
+        // 3) 최초 옵션 적용
         naverMapViewOptions.updateWithNaverMapView(
             naverMap: naverMap,
             isFirst: true
         )
 
-        // 4) 채널 핸들러 등록 (Dart에서 invokeMethod를 받을 부분)
+        // 4) Dart → Native 호출을 받을 handler 등록
         channel.setMethodCallHandler(handleMethodCall(_:result:))
 
-        // 5) 나머지 기존 로직
+        // 5) 나머지 onMapReady 로직
         onMapReady()
     }
 
@@ -52,23 +53,26 @@ internal class NaverMapView: NSObject, FlutterPlatformView {
     }
 
     deinit {
+        // 채널 정리
         (naverMapControlSender as! NaverMapController).removeChannel()
     }
 
+    // 지도 준비 완료 시
     private func onMapReady() {
         setMapTapListener()
         naverMapControlSender.onMapReady()
         deactivateLogo()
     }
 
+    // 네이버 로고 숨기기
     private func deactivateLogo() {
         let subviews = naverMap.mapView.subviews
-        // 네이버 로고 뷰 숨기기
         if subviews.count > 1 {
             subviews[1].isHidden = true
         }
     }
 
+    // 심볼 터치 리스너 등록
     private func setMapTapListener() {
         eventDelegate = NaverMapViewEventDelegate(
             sender: naverMapControlSender,
@@ -78,7 +82,7 @@ internal class NaverMapView: NSObject, FlutterPlatformView {
         eventDelegate.registerDelegates(mapView: naverMap.mapView)
     }
 
-    // 6) Dart → Native 호출을 실제로 처리하는 메서드
+    // 6) Dart → Native 호출 처리
     private func handleMethodCall(_ call: FlutterMethodCall,
                                   result: @escaping FlutterResult) {
         switch call.method {
@@ -93,14 +97,14 @@ internal class NaverMapView: NSObject, FlutterPlatformView {
                 ))
             }
 
-            // 7) 네이티브 API 호출: mapView에 레이어 토글
+            // iOS SDK에서는 setLayerGroup(_:isEnabled:) API 사용
             switch group {
             case "poi":
-                naverMap.mapView.setLayerGroupEnabled(.poi, enable)
+                naverMap.mapView.setLayerGroup(.poi, isEnabled: enable)
             case "transit":
-                naverMap.mapView.setLayerGroupEnabled(.transit, enable)
+                naverMap.mapView.setLayerGroup(.transit, isEnabled: enable)
             case "building":
-                naverMap.mapView.setLayerGroupEnabled(.building, enable)
+                naverMap.mapView.setLayerGroup(.building, isEnabled: enable)
             default:
                 break
             }
